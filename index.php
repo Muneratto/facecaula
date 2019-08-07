d64ed3946e786eae7d54cd9f9f155d5c8d3a63c7
Diogo Ferreira Muneratto <diogomuneratto@gmail.com>
2019-08-06 21:13:18 -0300

    projeto facec

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Marvel</title>
+
+    <link rel="stylesheet" href="asserts/css/bootstrap.min.css">
+    <link rel="stylesheet" href="asserts/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="asserts/css/site.css">
+
+    <script src="asserts/js/jquery-3.4.1.min.js"></script>
+    <script src="asserts/js/bootstrap.min.js"></script>
+    <script src="asserts/js/owl.carousel.min.js"></script>
+    <script src="asserts/js/function.js"></script>
+
+</head>
+<body>
+
+<header class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img class="logo" src="asserts/img/logo.png" title="logo do marvel" alt="logo do site"/>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Quem Somos</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Produtos</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Contato</a></li>
+        </ul>
+    </div>
+    </nav>
+</header>
+
+<div id="banner" class="container">
+    <div class="owl-carousel owl-theme">
+        <div class="item"><img src="/uploads/banner/banner.1.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
+        <div class="item"><img src="/uploads/banner/banner.2.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
+        <div class="item"><img src="/uploads/banner/banner.3.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
+    </div>
+</div>
+    
+</body>
+</html>
