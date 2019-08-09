60751db50baca3271ce09b4de32a77bd7ae8e3dd
Diogo Ferreira Muneratto <diogomuneratto@gmail.com>
2019-08-09 20:07:29 -0300

    segunda aula

--- index.php
+++ index.php
@@ -39,12 +39,106 @@
 </header>
 
 <div id="banner" class="container">
-    <div class="owl-carousel owl-theme">
+    <div class="owl-carousel-banner owl-carousel owl-theme">
         <div class="item"><img src="/uploads/banner/banner.1.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
         <div class="item"><img src="/uploads/banner/banner.2.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
         <div class="item"><img src="/uploads/banner/banner.3.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
     </div>
 </div>
-    
+
+<section id="cards" class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Lista de Produtos</h2>
+            <hr/>
+        </div>
+        <div class="col-md-4"> 
+            <div class="card">
+                <img src="/uploads/banner/banner.1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-danger w100">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4"> 
+            <div class="card">
+                <img src="/uploads/banner/banner.2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-danger w100">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4"> 
+            <div class="card">
+                <img src="/uploads/banner/banner.3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-danger w100">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+<section id="slide" class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Lançamentos</h2>
+            <hr/>
+        </div>
+        <div class="owl-carousel-slide owl-carousel owl-theme container">
+            <div class="item"> 
+                <div class="card">
+                    <img src="/uploads/banner/banner.1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-danger w100">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item"> 
+                <div class="card">
+                    <img src="/uploads/banner/banner.2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-danger w100">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item"> 
+                <div class="card">
+                    <img src="/uploads/banner/banner.3.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-danger w100">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item"> 
+                <div class="card">
+                    <img src="/uploads/banner/banner.3.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-danger w100">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
 </body>
 </html>
