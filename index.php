9c8bde3490242093908f4bf12f98aff54dd7d3f7
Diogo Muneratto <diogo@digitalone.com.br>
2019-08-16 18:12:25 -0300

    #new layout

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Marvel</title>
 
-    <link rel="stylesheet" href="asserts/css/bootstrap.min.css">
-    <link rel="stylesheet" href="asserts/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="asserts/css/site.css">
+    <link rel="stylesheet" href="./asserts/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./asserts/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="./asserts/css/site.css">
 
-    <script src="asserts/js/jquery-3.4.1.min.js"></script>
-    <script src="asserts/js/bootstrap.min.js"></script>
-    <script src="asserts/js/owl.carousel.min.js"></script>
-    <script src="asserts/js/function.js"></script>
+    <script src="./asserts/js/jquery-3.4.1.min.js"></script>
+    <script src="./asserts/js/bootstrap.min.js"></script>
+    <script src="./asserts/js/owl.carousel.min.js"></script>
+    <script src="./asserts/js/function.js"></script>
 
 </head>
 <body>
@@ -21,7 +21,7 @@
 <header class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">
-        <img class="logo" src="asserts/img/logo.png" title="logo do marvel" alt="logo do site"/>
+        <img class="logo" src="./asserts/img/logo.png" title="logo do marvel" alt="logo do site"/>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -40,9 +40,9 @@
 
 <div id="banner" class="container">
     <div class="owl-carousel-banner owl-carousel owl-theme">
-        <div class="item"><img src="/uploads/banner/banner.1.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
-        <div class="item"><img src="/uploads/banner/banner.2.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
-        <div class="item"><img src="/uploads/banner/banner.3.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
+        <div class="item"><img src="./uploads/banner/banner.1.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
+        <div class="item"><img src="./uploads/banner/banner.2.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
+        <div class="item"><img src="./uploads/banner/banner.3.jpg"  title="banner vingadores" alt="banner vingadores" /></div>
     </div>
 </div>
 
@@ -54,7 +54,7 @@
         </div>
         <div class="col-md-4"> 
             <div class="card">
-                <img src="/uploads/banner/banner.1.jpg" class="card-img-top" alt="...">
+                <img src="./uploads/banner/banner.1.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -64,7 +64,7 @@
         </div>
         <div class="col-md-4"> 
             <div class="card">
-                <img src="/uploads/banner/banner.2.jpg" class="card-img-top" alt="...">
+                <img src="./uploads/banner/banner.2.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -74,7 +74,7 @@
         </div>
         <div class="col-md-4"> 
             <div class="card">
-                <img src="/uploads/banner/banner.3.jpg" class="card-img-top" alt="...">
+                <img src="./uploads/banner/banner.3.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -96,7 +96,7 @@
         <div class="owl-carousel-slide owl-carousel owl-theme container">
             <div class="item"> 
                 <div class="card">
-                    <img src="/uploads/banner/banner.1.jpg" class="card-img-top" alt="...">
+                    <img src="./uploads/banner/banner.1.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -106,7 +106,7 @@
             </div>
             <div class="item"> 
                 <div class="card">
-                    <img src="/uploads/banner/banner.2.jpg" class="card-img-top" alt="...">
+                    <img src="./uploads/banner/banner.2.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -116,7 +116,7 @@
             </div>
             <div class="item"> 
                 <div class="card">
-                    <img src="/uploads/banner/banner.3.jpg" class="card-img-top" alt="...">
+                    <img src="./uploads/banner/banner.3.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -126,7 +126,7 @@
             </div>
             <div class="item"> 
                 <div class="card">
-                    <img src="/uploads/banner/banner.3.jpg" class="card-img-top" alt="...">
+                    <img src="./uploads/banner/banner.3.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -139,6 +139,29 @@
 
 </section>
 
+
+<footer>
+    <div class="container">
+        <div class="box"></div>
+        <div class="row">
+            <div class="col-md-3">
+                <img class="footer-logo" src="./asserts/img/logo.png"/>
+            </div>        
+            <div class="col-md-4">
+                <h2 class="footer-h2">Mapa do Site</h2>
+                <ul class="footer-menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Quem Somos</a></li>
+                    <li><a href="#">Produtos</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </div>        
+        </div>
+        <div class="box-bottom">
+            texto
+        </div>
+    </div>
+</footer>
 
 </body>
 </html>
